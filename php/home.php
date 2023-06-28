f2afeb893adaad4641fd0eab47279155f056a7d3
Joost-berkhout <260422@cursist.rockopnh.nl>
2023-06-28 10:59:17 +0200

    maak de css duidelijker

--- php/home.php
+++ php/home.php
@@ -4,11 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include "../php/components/header/header.php" ?>
-    
     <title>home</title>
 </head>
 <body>
-        
-    
 </body>
 </html>
