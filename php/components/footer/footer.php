f4cb1ae0b5a4c2fc63c8b9c339c8f03aebf6ab68
Joost-berkhout <260422@cursist.rockopnh.nl>
2023-06-30 13:22:29 +0200

    nummer updatesd  in de footer

--- php/components/footer/footer.php
+++ php/components/footer/footer.php
@@ -2,7 +2,7 @@
     <footer class="footer">
         <a href="../php/contact.php">Contact</a>
         <span class="vertical-stripe"></span>
-        <span class="phone-number"><a href="tel:+0644209559">Tel: 06 44 20 95 59</a></span>
+        <span class="phone-number"><a href="tel:+0644209559">06 44 20 95 59</a></span>
         <span class="vertical-stripe"></span>
-        <span class="email"><a href="mailto:joostberkhout.werk@gmail.com">Email: joostberkhout.werk@gmail.com</a></span>
+        <span class="email"><a href="mailto:joostberkhout.werk@gmail.com">joostberkhout.werk@gmail.com</a></span>
     </footer>
