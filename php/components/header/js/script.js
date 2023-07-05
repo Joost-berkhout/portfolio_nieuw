c90a7d495a8708da268976c0ad56ec896b41b268
Joost-berkhout <260422@cursist.rockopnh.nl>
2023-07-05 11:48:40 +0200

    fixing bug with contact form

--- php/components/header/js/script.js
+++ php/components/header/js/script.js
@@ -1,13 +1,13 @@
-// script.js
-
+//make navbar margin so there cant content in the navbar
 window.addEventListener('load', function() {
   adjustContentMargin();
 });
-
+//make navbar margin so there cant content in the navbar
 window.addEventListener('resize', function() {
   adjustContentMargin();
 });
 
+//make footer stay under the page
 function adjustContentMargin() {
   var navbarHeight = document.querySelector('.navbar').offsetHeight;
   document.querySelector('.content').style.marginTop = navbarHeight + 'px';
