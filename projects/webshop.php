3452f9c8242dc5a1da9e319e27b05817e3e6cda6
Joost-berkhout <260422@cursist.rockopnh.nl>
2023-07-15 17:33:22 +0200

    webshop text correction

--- projects/webshop.php
+++ projects/webshop.php
@@ -23,20 +23,20 @@
         <p>Geleerd:<br>
         PHP, CSS, Javascript en MySQL.<br>
         Ik begon met dit project aan een moodbord, nadat die was goedgekeurd ging ik door aan een wireframe.
-        Nadat bijde goed genoeg waren ben ik begonnen met het maken ervan.
+        Nadat beide goed genoeg waren ben ik begonnen met het maken ervan.
         De site lijkt heel erg op mijn wireframe maar jammergenoeg was de tijd te snel om.<br>
         Ik heb mijn kennis over PHP, CSS en MySQL verbeterd. graag zou ik toch meer aandacht aan Javascript geven.</p>
 
         <p>Wat zou ik anders doen:<br>
         De site afmaken, meer Javascript gebruiken en mobiel compatible.<br>
-        De site was door tijdsnood niet af, we hadden toen die tijd ook 2 projecten lopen met bijde veel nieuwe les stof.
+        De site was door tijdsnood niet af, we hadden toen die tijd ook 2 projecten lopen met beide veel nieuwe les stof.
         Ik vind het al knap dat ik met id uit database het voor elkaar kreeg om nieuwe pagina's te maken.
         Bij mijn volgende webshop zorg ik voor een betaal manier.<br>
         Als ik meer Javascript had gebruikt had de footer mooi onder site gekunt, maar toen die tijd wist ik nog niet veel van Javascript.
         Dus volgende keer ga ik de site mooier maken met Javascript.<br>
         Volgende keer ga ik ook de site mobiel belang geven, want je site moet op mobiel goed doen om publiek te trekken.</p>
 
-        <p>Al en al kan er veel verbeterd worden, maar het begin is er.
+        <p>Al met al kan er veel verbeterd worden, maar het begin is er.
         Ik heb in dit project veel geleerd voor de volgende keer.
         </span>  
     </div>
